a989d2dd67e3361c71e9c4e2573ce80434e833ce
Glenn Sandoval <glenn.sandoval@gmail.co,m>
2021-12-14 12:06:38 +1300

    initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from db import Session, engine, Base
+from user import User
+
+if __name__ == '__main__':
+    Base.metadata.create_all(engine)
+    session = Session()
+    test_user = User(name="Dave", fullname="Davothy", nickname="Sam")
+    session.add(test_user)
+    session.commit()
+    our_user = session.query(User).filter_by(name='Dave').first()
+    print(our_user)
