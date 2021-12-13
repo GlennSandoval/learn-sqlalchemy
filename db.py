e2fd4120bab06053db124c29fa00f8849817d3ee
Glenn Sandoval <glenn.sandoval@gmail.co,m>
2021-12-14 12:14:42 +1300

    store db in file for inspection

--- db.py
+++ db.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-engine = create_engine('sqlite:///:memory:', echo=True)
+engine = create_engine('sqlite:///data.db')
 Base = declarative_base()
 Session = sessionmaker(bind=engine)
